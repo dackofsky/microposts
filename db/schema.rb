97bdb6c689f64f1e9b06edf08d949ff4c5bc6210
EIJI MORI <dackofsky77@yahoo.co.jp>
2015-09-05 05:38:16 +0000

    add sign sign up and user detail

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20150903230746) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "passwor_digest"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
