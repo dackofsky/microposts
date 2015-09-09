11d5a96d70748754a814cada1ab87f9fc3720b24
EIJI MORI <dackofsky77@yahoo.co.jp>
2015-09-09 06:47:24 +0000

    add edit2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,11 +35,11 @@ class UsersController < ApplicationController
   
   def set_user
     @user = User.find(params[:id])
+  end
   
   def user_params
     params.require(:user).permit(:name, :country, :email, :password, :password_confirmation)
     
-  end
   end
 end
 
